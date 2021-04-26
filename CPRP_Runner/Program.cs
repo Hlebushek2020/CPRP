6115614ac308f8550a71ab64e264718940d7698f
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2021-04-26 19:51:04 +0300

    Init

---
+++ CPRP_Runner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRP_Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
