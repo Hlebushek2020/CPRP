1190796155752f31abec23f05a82be328b26eff2
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2021-04-27 21:45:56 +0300

    Update 27-04-2021

--- CPRP_Runner/Program.cs
+++ CPRP_Runner/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,43 @@ namespace CPRP_Runner
     {
         static void Main(string[] args)
         {
+            Process runApp = new Process();
+            // set app
+            runApp.StartInfo.FileName = args[0];
+            // construct args for app
+            for (int i = 1; i < args.Length - 1; i++)
+            {
+                if (File.Exists(args[i]))
+                    runApp.StartInfo.Arguments += $"\"{args[i]}\" ";
+                else
+                    runApp.StartInfo.Arguments += args[i] + " ";
+            }
+            runApp.StartInfo.Arguments = runApp.StartInfo.Arguments.TrimEnd(' ');
+            // start
+            runApp.Start();
+            // set priority
+            string priorityArg = args[args.Length - 1];
+            switch (priorityArg)
+            {
+                case "-n": //Normal = 32
+                    runApp.PriorityClass = ProcessPriorityClass.Normal;
+                    break;
+                case "-i": //Idle = 64
+                    runApp.PriorityClass = ProcessPriorityClass.Idle;
+                    break;
+                case "-h": //High = 128
+                    runApp.PriorityClass = ProcessPriorityClass.High;
+                    break;
+                case "-rt": //RealTime = 256
+                    runApp.PriorityClass = ProcessPriorityClass.RealTime;
+                    break;
+                case "-bn": //BelowNormal = 16384
+                    runApp.PriorityClass = ProcessPriorityClass.BelowNormal;
+                    break;
+                case "-an": //AboveNormal = 32768
+                    runApp.PriorityClass = ProcessPriorityClass.AboveNormal;
+                    break;
+            }
         }
     }
 }
